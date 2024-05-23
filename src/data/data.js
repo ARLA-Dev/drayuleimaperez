c850be6a3995fa182a36ee89e9d0c35cd1c247a7
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-05-23 00:46:17 -0400

    Se termina la sección del AboutMe

---
+++ src/data/data.js
@@ -0,0 +1,17 @@
+export const cards = [
+    {
+        title: "Médico Especialista En Ginecología Y Obstetricia",
+        description: "Graduada en la Universidad Centro Occidental Lisandro Alvarado de Barquisimeto.",
+        img: "logoUCLA"
+    },
+    {
+        title: "Dedicación En Cada Caso Médico",
+        description: "Cada paciente es tratada con atención personalizada y la calma necesaria para que su viaje de salud sea lo más cómodo posible.",
+        img: "dedicacion"
+    },
+    {
+        title: "Entendimiento Entre Ambas Partes",
+        description: "Como tu médico, intentaré explicar hasta los términos médicos más díficiles asociados a tu condición para que puedas tener una mejor comprensión de lo que sucede en tu cuerpo.",
+        img: "entendimiento"
+    }
+]
